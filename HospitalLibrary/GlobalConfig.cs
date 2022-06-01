afaddbd41ac27107c124b420d6e7dde3b8d2d13c
Furkan <45736470+nihain@users.noreply.github.com>
2022-06-01 21:32:46 +0300

    create database interfaces

---
+++ HospitalLibrary/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO: Create SQL connection
+            }
+            if (textFiles)
+            {
+                // TODO: Create text file connection
+            }
+        }
+    }
+}
