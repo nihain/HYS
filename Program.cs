a66d5b7176cd53721ca727d9e1e1acb93aba02c9
Furkan <45736470+nihain@users.noreply.github.com>
2022-06-02 12:45:31 +0300

    remove dependency

--- Program.cs
+++ Program.cs
@@ -15,7 +15,6 @@ namespace HYS
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize the database connections
-            
 
             Application.Run(new FormLoginRegister());
         }
